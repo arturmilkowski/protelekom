7cb52641cf8ff42c6afa1a6a5218d04e23f7225d
Artur M <artur-milkowski@tlen.pl>
2020-11-29 15:38:00 +0100

    Dodanie cache 1

--- app/Http/Controllers/Frontend/Product/Category/Show.php
+++ app/Http/Controllers/Frontend/Product/Category/Show.php
@@ -5,12 +5,15 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Frontend\Product\Category;
 
 use App\Http\Controllers\Controller;
-// use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\View\View;
 use App\Models\Category;
 
 class Show extends Controller
 {
+    const SECONDS = 60 * 60 * 12;  // 12 = 12 hours. 168 = one week
+
     /**
      * Show products in category.
      *
@@ -20,25 +23,30 @@ class Show extends Controller
      */
     public function __invoke(Category $category): View
     {
-        // TODO change this for query
-        $products = $category->products;                
-        /*
-        $products = DB::table('brands')
-            ->join('products', 'products.brand_id', '=', 'brands.id')
-            ->join('categories', 'products.category_id', '=', 'categories.id')
-            ->where('category_id', $category->id)
-            ->where('products.hide', 0)
-            ->distinct()
-            ->select(
-                'products.id as id',
-                'brands.id as brand_id', 'brands.name as brand_name',
-                'categories.id as category_id', 'categories.name as category_name',
-                'products.slug', 'products.name', 'products.description',
-                'products.img', 'products.site_description', 'products.site_keyword',
-                'products.hide'
-            )            
-            ->get();
-        */
+        // $products = $category->products;
+        
+        $products = Cache::remember(
+            'products',
+            self::SECONDS,
+            function () use ($category) {
+                return DB::table('brands')
+                    ->join('products', 'products.brand_id', '=', 'brands.id')
+                    ->join('categories', 'products.category_id', '=', 'categories.id')
+                    ->where('category_id', $category->id)
+                    ->where('products.hide', 0)
+                    ->distinct()
+                    ->select(
+                        'products.id as id',
+                        'brands.id as brand_id', 'brands.name as brand_name',
+                        'categories.id as category_id', 'categories.name as category_name',
+                        'products.slug', 'products.name', 'products.description',
+                        'products.img', 'products.site_description', 'products.site_keyword',
+                        'products.hide'
+                    )            
+                    ->get();
+            }
+        );
+        
         $currentRouteName = 'frontend.product';
         
         return view(
