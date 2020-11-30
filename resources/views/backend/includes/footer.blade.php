d76cfb316294197d3f269c141e610e32dcd75754
Artur M <artur-milkowski@tlen.pl>
2020-11-30 10:39:01 +0100

    Informacja o wersji frameworka i php w stopce

--- resources/views/backend/includes/footer.blade.php
+++ resources/views/backend/includes/footer.blade.php
@@ -7,10 +7,12 @@
                         action: <strong>{{ Route::currentRouteAction() }}</strong> &bull; route name: <strong>{{ Route::currentRouteName() }}</strong>
 @auth
                         &bull; user: <strong>{{ Auth::user()->name }}</strong> &bull; uprawnienia: <strong>{{ Auth::user()->role->display_name }}</strong>
+                        &bull; laravel {{ Illuminate\Foundation\Application::VERSION }} 
+                        &bull; php {{ PHP_VERSION }}
 @endauth
 @else
-                        <div class="float-left text-black-50">2020 &mdash; {{ config('settings.company_name') }}</div>
-                        <div class="float-right text-black-50">wersja 0.0.1</div>
+                        <div class="float-left text-black-50">2000 &mdash; {{ date("Y") }} {{ config('settings.company_name') }}</div>
+                        <div class="float-right text-black-50">l.{{ Illuminate\Foundation\Application::VERSION }} p.{{ PHP_VERSION }}</div>
 @endif
                     </small>
                 </div>
